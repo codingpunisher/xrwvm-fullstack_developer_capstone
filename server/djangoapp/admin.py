92494f6cd09bb029996e3b56d41838a23794ee5c
justus <jjaramilloapps@gmail.com>
2024-04-23 15:27:31 -0400

    admin.py pylint 1

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -7,18 +7,28 @@ from .models import CarMake, CarModel
 # CarModelInline class
 class CarModelInline(admin.TabularInline):
     model = CarModel
-    extra = 1  # Specifies the number of blank forms to display at the end of the inline.
+    extra = 1  # Specifies the number of blank forms to display 
+    #at the end of the inline.
+
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'year','car_make')  # Columns shown in the admin list view
-    list_filter = ('type', 'year','car_make')  # Filters available on the sidebar
-    search_fields = ('name', 'type', 'make__name')  # Searchable fields
+     # Columns shown in the admin list view
+    list_display = ('name', 'type', 'year', 'car_make')
+     # Filters available on the sidebar 
+    list_filter = ('type', 'year', 'car_make')
+    # Searchable fields
+    search_fields = ('name', 'type', 'make__name')  
+
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ('name', 'description', 'created_at', 'updated_at')
     search_fields = ('name', 'description')
     list_filter = ('created_at', 'updated_at')
+
+
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
